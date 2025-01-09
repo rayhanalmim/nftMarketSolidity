63a6eef4a99656e99c90dc5b2d540c38d62cd9de
rayhanalmim <rayhanalmim1@gmail.com>
2025-01-09 11:05:25 +0600

    Add Foundry configuration and usage instructions in forgekey.sol

---
+++ Constants/forgekey.sol
@@ -0,0 +1,28 @@
+Your public key has been saved in /home/rayhan/.ssh/id_ed25519.pub
+The key fingerprint is:
+SHA256:AglzDuQaZovPwEHoJoVlWvHO9CBFPUZMS7p80Ft48os rayhanalmim1@gmail.com
+The key's randomart image is:
++--[ED25519 256]--+
+|.=Xo+=+          |
+|+=.O ==o         |
+|+=o X.=.o        |
+|*=o* * *         |
+|*o  = = S        |
+| +   . o .       |
+|  o   E .        |
+|                 |
+|                 |
++----[SHA256]-----+
+
+
+
+
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPny8D3JAEBoYE7wLwSPBdspirSQUgMskMNyc3dgpe6 rayhanalmim1@gmail.com
+
+foundry code - forge
+forge compile
+anvil
+forge --help
+deploy : forge create --help
+forge create SimpleStorage (default)
+intract with the custom chain : forge crate SimpleStorage --rpc-url http://... --interactive
