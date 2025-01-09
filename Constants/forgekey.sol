1c2a7fd492962ccc7bdc37564c8c7d5abdf12b07
rayhanalmim <rayhanalmim1@gmail.com>
2025-01-09 12:45:57 +0600

    Update onchain deployment script instructions in forgekey.sol for production usage

--- Constants/forgekey.sol
+++ Constants/forgekey.sol
@@ -30,7 +30,7 @@ forge create SimpleStorage --rpc-url http://127.0.0.1:8545 --interactive --broad
 
 clear history : history -c
 
-onchain deployment script : forge script script/DeploySimpleStorage.s.sol --rpc-url http://127.0.0.1:8545/ --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+onchain deployment script : forge script script/DeploySimpleStorage.s.sol --rpc-url http://127.0.0.1:8545/ --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 (FOR production must be used : --interactive)
 
 hax to dec : cast --to-base 0x714c2 dec
 case --help
